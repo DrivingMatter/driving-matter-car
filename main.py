fa99aa27124113c1448af8107de03449d0744242
DMRL <drivingmatter@gmail.com>
2017-11-01 19:21:40 +0000

    Settings in main.py

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ import pkgutil
 from classes.Server import Server
 from classes.Car4W import Car4W
 from classes.Tyre import Tyre
+from classes.Camera import Camera
 from classes.UDSensor import CollisionSensor
-from handlers import Action, CameraOne, State
+from handlers import Action, CameraC, State
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 logging.basicConfig(level=logging.DEBUG)
 
 # Loading config file
 config = json.loads(open("config.json").read())
+
+car_speed = config['car_speed']
+timeframe = config['timeframe']
+port = config['port']
 
 # Loading Sensors Settings
 ss = sensor_settings = config['sensor_settings']
@@ -32,34 +37,36 @@ sensors = [("center", ultrasonic_c), ("left", ultrasonic_l), ("right", ultrasoni
 
 # Loading Tyre Settings
 ts = tyre_settings = config['tyre_settings']
-frontRight = Tyre(ts['front_right']['forwardPin'], ts['front_right']['backwardPin'], ts['front_right']['pwmPin'], ts['car_speed'])
-frontLeft = Tyre(ts['front_left']['forwardPin'], ts['front_left']['backwardPin'], ts['front_left']['pwmPin'], ts['car_speed'])
-backRight = Tyre(ts['back_right']['forwardPin'], ts['back_right']['backwardPin'], ts['back_right']['pwmPin'], ts['car_speed'])
-backLeft = Tyre(cotsnfig['back_left']['forwardPin'], ts['back_left']['backwardPin'], ts['back_left']['pwmPin'], ts['car_speed'])
+frontRight = Tyre(ts['front_right']['forwardPin'], ts['front_right']['backwardPin'], ts['front_right']['pwmPin'], car_speed)
+frontLeft = Tyre(ts['front_left']['forwardPin'], ts['front_left']['backwardPin'], ts['front_left']['pwmPin'], car_speed)
+backRight = Tyre(ts['back_right']['forwardPin'], ts['back_right']['backwardPin'], ts['back_right']['pwmPin'], car_speed)
+backLeft = Tyre(ts['back_left']['forwardPin'], ts['back_left']['backwardPin'], ts['back_left']['pwmPin'], car_speed)
 tyres = [("fr", frontRight), ("fl", frontLeft), ("br", backRight), ("bl", backLeft)]
 
 # Loading camera settings and camera
 cs = camera_settings = config['camera_settings']
+
 cameras = []
 if camera_settings:
     csc = camera_settings_c = camera_settings.get('camera_c')
     if camera_settings_c:
-        camera_c = Camera(csc['camera_type'], csc['camera_num'], csc['resolution'], csc['framerate'], csc['resolution'])
+        camera_c = Camera(csc['camera_type'], csc['camera_num'], csc['resolution'], csc['framerate'], csc['rotation'])
         cameras.append(("center", camera_c))
 
-timeframe = config['timeframe']
 
 car = Car4W(tyres, sensors, camera_settings, timeframe)
 
-handlers = [
-    (r"/camera_c", handlers.CameraC),
-    (r"/action", handlers.Action, {'car': car}),
-    (r"/state", handlers.State, {'car': car})
+h = [
+    (r"/camera_c", CameraC.CameraC),
+    (r"/action", Action.Action, {'car': car}),
+    (r"/state", State.State, {'car': car})
 ]
 
-try:
-    logging.debug("main.py called")
-    s = Server(handlers, port=config['port'])
-    s.start()
-except KeyboardInterrupt:
-    print "Exiting";
+if __name__ == "__main__":
+	
+	try:
+	    logging.debug("main.py called")
+	    s = Server(h, port=port)
+	    s.start()
+	except KeyboardInterrupt:
+	    print "Exiting";
