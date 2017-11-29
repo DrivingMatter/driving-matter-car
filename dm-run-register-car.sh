c82d67f6dc4db86dc4de8fecccac0c7285e1ba73
Syed Owais Ali Chishti <soac@outlook.com>
2017-11-30 02:01:25 +0500

    Refactor 003

--- dm-run-register-car.sh
+++ dm-run-register-car.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # How to run this script.
 # sudo crontab -e
-# @reboot /home/pi/Desktop/driving-matter-car/scripts/dm-run-register-car.sh
+# @reboot /home/pi/Desktop/driving-matter-car/dm-run-register-car.sh
 # 
 cd /home/pi/Desktop/driving-matter-car/classes/
 /usr/bin/python /home/pi/Desktop/driving-matter-car/classes/RegisterCar.py &
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import time
-import threading
-import io
-import picamera
-import tornado.web
-import tornado.websocket
 import logging
 import signal
 import sys
-import json
-import os
-import pkgutil
-import socket
 
 from classes.Server import Server
 from classes.LoadCar import load_car
@@ -20,7 +10,7 @@ from handlers import Action, State
 # Work only on python 2 because using pickle and bytes difference in between python versions.
 assert(sys.version_info.major == 2)
 
-logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 car = load_car("config.json")
