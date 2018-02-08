92e35ead27ff40e30e0f6ceed61dbb1c5e55c6c9
DrivingMatter <drivingmatter@gmail.com>
2018-02-08 22:56:15 +0000

    4-5 fps

--- classes/Driver.py
+++ classes/Driver.py
@@ -22,7 +22,7 @@ class Driver:
         self.cv2_window_name = []
         
     def detect_sign(self, datavector):
-        i=0
+        
         sign_detection= SignDetection()
         camera_names = [key for key in datavector if key.startswith('camera')]
         for name in camera_names:
@@ -31,7 +31,12 @@ class Driver:
                 self.cv2_window_name.append(name)
 
             frame = datavector[name]
+            width, height, channel = frame.shape
+            frame=cv2.resize(frame,(int(width),int(height/2)),interpolation=cv2.INTER_AREA)
             frame = sign_detection.detect(frame)
+            #frame=cv2.resize(frame,(int(width),int(height*2)),interpolation=cv2.INTER_AREA)
+            print(time.time())
+            
             cv2.imshow(name, frame)
             cv2.waitKey(1)  # CV2 Devil - Don't dare to remove
             
